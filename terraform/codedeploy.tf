11ef8d74bd425bab355ddaa09a79932373049cef
Cristian Rubio <cristianrubioa@gmail.com>
2026-05-08 14:07:01 -0500

    test: broken SSM param to trigger failed CD scenario

---
+++ terraform/codedeploy.tf
@@ -0,0 +1,51 @@
+resource "aws_codedeploy_app" "main" {
+  name             = "${local.name_prefix}-deploy"
+  compute_platform = "ECS"
+  tags             = local.tags
+}
+
+resource "aws_codedeploy_deployment_group" "main" {
+  app_name               = aws_codedeploy_app.main.name
+  deployment_group_name  = "${local.name_prefix}-deploy-group"
+  service_role_arn       = aws_iam_role.codedeploy.arn
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout    = "STOP_DEPLOYMENT"
+      wait_time_in_minutes = 5
+    }
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+  }
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.app.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http.arn]
+      }
+      test_traffic_route {
+        listener_arns = [aws_lb_listener.test.arn]
+      }
+      target_group { name = aws_lb_target_group.blue.name }
+      target_group { name = aws_lb_target_group.green.name }
+    }
+  }
+}
